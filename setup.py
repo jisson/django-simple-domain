279ee5e268931e2f87f840d371865806b1722be4
pierre <pierre.rodier@gmail.com>
2016-11-18 16:11:25 +0100

    Updated to 0.5:: Added compatibility with Django 1.10

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ __author__ = 'Jisson | pierre@jisson.com'
 
 setup(name='django-simple-domain',
       packages=['django_simple_domain'],
-      version='0.3',
+      version='0.5',
       url='https://github.com/jisson/django-simple-domain',
-      download_url='https://github.com/jisson/django-simple-domain/tarball/0.3',
+      download_url='https://github.com/jisson/django-simple-domain/tarball/0.5',
       license='Public',
       author='Jisson',
       author_email='pierre@jisson.fr',
