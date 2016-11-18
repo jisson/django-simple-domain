aad07546fc641786c5b7c9089d4cacc56151955f
pierre <pierre.rodier@gmail.com>
2016-11-18 14:41:42 +0100

    Removed unused method from settings. That method was commented

--- django_simple_domain/django_simple_domain_settings.py
+++ django_simple_domain/django_simple_domain_settings.py
@@ -11,14 +11,3 @@ DEACTIVATING_COMMANDS = getattr(settings, 'SIMPLE_DOMAIN_DEACTIVATING_COMMANDS',
 
 # Retrieving SITE_ID from global settings
 SITE_ID = getattr(settings, 'SITE_ID', None)
-
-
-# TODO: Check if that method is really use full. TODO: Seems to be not used finally
-# def set_site_id(site_id):
-#     """
-#     Accessor to simplify attribution of new values to SITE_ID directly from 'django_simple_domain_settings'.
-#
-#     :param  site_id:    The new value to assign to SITE_ID property
-#     :type   site_id:    int
-#     """
-#     settings.SITE_ID._set(site_id)
